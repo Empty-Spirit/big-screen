8ff60fe15331f70e4e6e853670c5f62af2bda3f2
zzx <19957296561@163.com>
2021-06-09 16:17:55 +0800

    rem适配

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,17 +4,33 @@
       alt="Vue logo"
       src="../assets/logo.png"
     >
-    <HelloWorld msg="Welcome to Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <div class='test'></div>
+    <div class='test2'></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../styles/rem.scss';
+.test {
+  width: toRem(100);
+  height: toRem(100);
+  background: #000;
+}
+.test2 {
+  width: 100px;
+  height: 100px;
+  background: #000;
+}
+</style>
