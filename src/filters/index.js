5e869fe7ee0876b5e07fbfa6a340bd024df56795
zzx <19957296561@163.com>
2021-06-17 17:56:28 +0800

    金额转换封装g

--- src/filters/index.js
+++ src/filters/index.js
@@ -2,8 +2,11 @@
 import Vue from 'vue'
 import timestamp from './timestamp'
 import checkStatus from './checkStatus'
+import money from './money'
 
 Vue.filter('timestamp', timestamp)
 Vue.prototype.$timestamp = timestamp
 Vue.filter('checkStatus', checkStatus)
 Vue.prototype.$checkStatus = checkStatus
+Vue.filter('money', money)
+Vue.prototype.$money = money
